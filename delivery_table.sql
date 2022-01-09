859ef94bfd1841df2ad1ef89e4ca4596b852b72d
Peter Finnerty <finnertypete@gmail.com>
2022-01-09 22:10:14 +0000

    Finished README. Added dbconfig.py and template, ammended DAO file to import configuration file. Added SQL file containing the CREATE and INSERT functions for the delivery table.

---
+++ delivery_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE delivery (id int PRIMARY KEY AUTO_INCREMENT, Item varchar(255), Type Varchar(255), Quantity int, TotPrice int);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("Denny Sausages", "Food", 50, 30);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("2L full-fat milk", "Food", 30, 14);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("Pat the Baker Bread", "Food", 40, 32);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("headphones", "electronics", 20, 228);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("USB cables", "electronics", 26, 146);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("Four Four Two", "magazine", 30, 56);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("The Irish Times", "newspaper", 46, 53);
+
+INSERT INTO delivery (Item, Type, Quantity, Totprice) VALUES ("Twix", "sweets", 38, 34);
