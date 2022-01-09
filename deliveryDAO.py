7e0def5d8739d9514f40f213d14689e56be112f2
Peter Finnerty <finnertypete@gmail.com>
2022-01-09 00:00:37 +0000

    Added update, create and delete functions to DAO. Also added convertToDict function to change tuples into dictionary for JSON conversion.

--- deliveryDAO.py
+++ deliveryDAO.py
@@ -10,18 +10,18 @@ class DeliveryDAO:
         database="delivery"
         )
   
-    def findAll(self):
+        def findAll(self):
         cursor = self.db.cursor()
         sql="select * from delivery"
         cursor.execute(sql)
         results = cursor.fetchall()
-        itemArray = []
+        returnArray = []
         print(results)
         for result in results:
             print(result)
-            itemArray.append(result)
+            returnArray.append(self.convertToDict(result))
 
-        return itemArray  
+        return returnArray
         
     def findItem(self, id):
         cursor = self.db.cursor()
@@ -30,8 +30,43 @@ class DeliveryDAO:
 
         cursor.execute(sql, values)
         result = cursor.fetchone()
-        return result
+        return self.convertToDict(result)
+            
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into delivery (Item, Type, Quantity, TotPrice) values (%s,%s,%s,%s)"
+        cursor.execute(sql, values)
 
+        self.db.commit()
+        return cursor.lastrowid
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql="update delivery set Item=%s, Type=%s, Quantity=%s, TotPrice=%s  where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from delivery where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDict(self, result):
+        colnames=['id','Item','Type', 'Quantity',"TotPrice"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
 deliveryDAO = DeliveryDAO()
 
 
